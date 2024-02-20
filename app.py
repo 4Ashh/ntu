0963b5cf72e8fca78c6cffd0b5494c45e0d05805
4Ashh <132888875+4Ashh@users.noreply.github.com>
2024-02-20 15:26:25 +0800

    Update app.py

--- app.py
+++ app.py
@@ -4,16 +4,16 @@ app = Flask(__name__)
 
 @app.route("/",methods=["GET","POST"])
 def index():
-  return(render_template("index.html"))
+    return(render_template("index.html"))
 
 @app.route("/main",methods=["GET","POST"])
 def main():
-  r = request.form.get("r")
-  return(render_template("main.html",r=r))
+    r = request.form.get("r")
+    return(render_template("main.html",r=r))
 
-@app.route("/imageGPT",methods=["GET","POST"])
-def main():
-  return(render_template("imageGPT.html"))
+@app.route("/image_gpt",methods=["GET","POST"])
+def image_gpt():
+    return(render_template("image_gpt.html"))
 
 if __name__ == "__main__":
-  app.run()
+    app.run()
